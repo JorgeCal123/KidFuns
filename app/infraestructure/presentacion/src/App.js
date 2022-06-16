9236855dfdacb5abca8be0f6a052b946294b6ec5
jhonpulido1990 <3825@holbertonschool.com>
2022-06-16 17:01:43 -0500

    primer avance

---
+++ app/infraestructure/presentacion/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from './image/mainimagen.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="hola"/>
+        <div id="fb-root"></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
