97a66f5d26b70d1f3f20fc8c17506ea2e8c4c468
jhonpulido1990 <3825@holbertonschool.com>
2022-06-20 16:42:44 -0500

    add formulario

--- app/infraestructure/presentacion/src/Logo.js
+++ app/infraestructure/presentacion/src/Logo.js
@@ -1,17 +1,11 @@
 import React from "react";
 import logo1 from './image/mainimagen.png';
-import { Search } from "./Search"
 
 function Logo() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo1} alt="hola" />
-                
-                <div>
-                    <button>Search</button>
-                    <Search />
-                </div>
             </header>
         </div>
     );
