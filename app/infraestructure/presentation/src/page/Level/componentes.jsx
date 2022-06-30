237c26c392d667a85f1cdff8b838042af37105ce
jhonpulido1990 <3825@holbertonschool.com>
2022-06-30 12:46:47 -0500

    add level code

--- app/infraestructure/presentation/src/page/Level/componentes.jsx
+++ app/infraestructure/presentation/src/page/Level/componentes.jsx
@@ -1,19 +1,6 @@
-import logo from '../../image/logos/logo7-4.PNG'
-import lettera from '../../image/abecedario/a.png'
-import micro from '../../image/logos/microphone.png'
-import speaker from '../../image/logos/speaker.png'
-import speaker1 from '../../image/logos/speaker2.png'
-import conejo from '../../image/animales/conejo4.png'
-import conejo1 from '../../image/animales/conejo5.png'
-import conejo2 from '../../image/animales/conejo6.png'
-import marco from '../../image/recursos/marco.png'
-import CanvasDraw from 'react-canvas-draw'
-import { BotonCentrado, Formulario } from '../../elementos/Formularios1'
-function WeekDays(props) {
-    switch (props.value) {
-        case 'a1':
-            return (
-            <main className='level1'>
+function Component1 ({logo, letter, speaker, Formulario, micro, conejo }) {
+    return (
+        <main className='level1'>
                 <div>
                     <header className="App-header">
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
@@ -24,7 +11,7 @@ function WeekDays(props) {
                         <p class="text_shadows">Repite</p>
                     </div>
                     <Formulario>
-                            <img src={ lettera} alt='Kidfuns' className='lettera'/>
+                            <img src={ letter } alt='Kidfuns' className='lettera'/>
                             <div>
                                  <img src={ speaker } alt='Kidfuns' className='speaker1'/>
                             </div>
@@ -39,10 +26,12 @@ function WeekDays(props) {
                     <img src={ conejo } alt='Kidfuns' className='logoprincipal'/>
                 </div>
             </main>
-            )
-        case 'a2':
-            return (
-            <main className='level1'>
+    );
+}
+
+function Component2({ logo, speaker1, BotonCentrado, conejo1 }){
+    return(
+        <main className='level1'>
                 <div>
                     <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
@@ -61,10 +50,12 @@ function WeekDays(props) {
                     <img src={ conejo1 } alt='Kidfuns' className='logoprincipal'/>
                 </div>
             </main>
-            )
-        case 'a3':
-            return ( 
-            <main className='level1'>
+    );
+}
+
+function Component3({ logo, lettera, marco, conejo2, BotonCentrado, CanvasDraw}) {
+    return (
+        <main className='level1'>
                 <div>
                     <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
@@ -82,10 +73,8 @@ function WeekDays(props) {
                 <div>
                     <img src={ conejo2 } alt='Kidfuns' className='logoprincipal'/>
                 </div>
-            </main>)
-        default:
-            return <h2>Invalid Input</h2>
-    }
+            </main>
+    );
 }
 
-export { WeekDays };
+export { Component1, Component2, Component3 };
---
+++ app/infraestructure/presentation/src/page/Level/level3.jsx
@@ -0,0 +1,18 @@
+import { Component3 } from "./componentes";
+import logo from '../../image/logos/logo7-4.PNG'
+import lettera from '../../image/abecedario/a.png'
+import conejo2 from '../../image/animales/conejo6.png'
+import marco from '../../image/recursos/marco.png'
+import CanvasDraw from 'react-canvas-draw'
+import { BotonCentrado } from '../../elementos/Formularios1'
+
+function Level3({ materia }) {
+    switch (materia) {
+        case 'm1':
+            return (<Component3 logo={logo} lettera={ lettera } marco={ marco } CanvasDraw={ CanvasDraw } BotonCentrado={ BotonCentrado } conejo2={conejo2}/>);
+        default:
+            return <h2>Invalid Input</h2>
+    }
+} 
+
+export { Level3 };
