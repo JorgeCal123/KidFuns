3209277591802710c7afc49f3fb56dbc08993aad
jhonpulido1990 <3825@holbertonschool.com>
2022-07-02 08:51:03 -0500

    order materia number

--- app/infraestructure/presentation/src/page/Level/componentes.jsx
+++ app/infraestructure/presentation/src/page/Level/componentes.jsx
@@ -112,63 +112,49 @@ function Component4 ({ logo, numero, mano, micro, speaker, Formulario, conejo })
     );
 }
 
-function Component5 ({logo, letter, speaker, Formulario, micro, conejo }) {
+function Component5 ({ logo, speaker1, BotonCentrado, conejo1}) {
     return (
         <main className='level1'>
                 <div>
-                    <header className="App-header">
+                    <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
                     </header>
                 </div>
-                <div className='level1'>
-                    <div class="content">
-                        <p class="text_shadows">Repite</p>
-                    </div>
-                    <Formulario>
-                            <img src={ letter } alt='Kidfuns' className='lettera'/>
-                            <div>
-                                 <img src={ speaker } alt='Kidfuns' className='speaker1'/>
-                            </div>
-                           
-                    </Formulario>
-                    <div class="content">
-                        <p class="text_shadows">Presiona</p>
-                    </div>
-                    <button type='button' className='button5'><img src={ micro } alt='Kidfuns' className='speaker1'/></button>
-                </div>
+                <BotonCentrado>
+                    <BotonCentrado>
+                        <img src={ speaker1 } alt='Kidfuns'/>
+                        <p>Selecciona la correcta</p>
+                        <button type="button">3</button>
+                        <button type="button">1</button>
+                        <button type="button">2</button>
+                    </BotonCentrado>
+                </BotonCentrado>
                 <div>
-                    <img src={ conejo } alt='Kidfuns' className='logoprincipal'/>
+                    <img src={ conejo1 } alt='Kidfuns' className='logoprincipal'/>
                 </div>
             </main>
     );
 }
 
-function Component6 ({logo, letter, speaker, Formulario, micro, conejo }) {
+function Component6 ({ logo, numero, marco, CanvasDraw, BotonCentrado, conejo2 }) {
     return (
         <main className='level1'>
                 <div>
-                    <header className="App-header">
+                    <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
                     </header>
                 </div>
-                <div className='level1'>
-                    <div class="content">
-                        <p class="text_shadows">Repite</p>
-                    </div>
-                    <Formulario>
-                            <img src={ letter } alt='Kidfuns' className='lettera'/>
-                            <div>
-                                 <img src={ speaker } alt='Kidfuns' className='speaker1'/>
-                            </div>
-                           
-                    </Formulario>
-                    <div class="content">
-                        <p class="text_shadows">Presiona</p>
-                    </div>
-                    <button type='button' className='button5'><img src={ micro } alt='Kidfuns' className='speaker1'/></button>
-                </div>
+                <BotonCentrado>
+                    <BotonCentrado>
+                        <img src={ numero } alt='Kidfuns' className='logoa'/>
+                        <p>Dibuja</p>
+                        <div>
+                            <CanvasDraw brushRadius={3} brushColor={'red'} imgSrc={ marco }/>
+                        </div>
+                    </BotonCentrado>
+                </BotonCentrado>
                 <div>
-                    <img src={ conejo } alt='Kidfuns' className='logoprincipal'/>
+                    <img src={ conejo2 } alt='Kidfuns' className='logoprincipal'/>
                 </div>
             </main>
     );
