925b262618e5b439ee9b94533e988713518a6112
jhonpulido1990 <3825@holbertonschool.com>
2022-07-04 01:14:43 -0500

    add excesice selection and style button and letter

--- app/infraestructure/presentation/src/page/Level/componentes.jsx
+++ app/infraestructure/presentation/src/page/Level/componentes.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import '../../estilos_viejos.css'
+
+function generateRandomLetter() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * (10 - 1 + 1)) + 0;
+}
+
+function generateRandomIndex() {
+    return Math.floor(Math.random() * (3 - 1 + 1)) + 0;
+}
+
 function Component1 ({logo, letter, speaker, Formulario, micro, conejo }) {
     return (
         <main className='level1'>
@@ -30,20 +48,42 @@ function Component1 ({logo, letter, speaker, Formulario, micro, conejo }) {
 }
 
 function Component2({ logo, speaker1, BotonCentrado, conejo1 }){
+    const a = generateRandomLetter()
+    const b = generateRandomLetter()
+    const c = generateRandomLetter()
+    const list = [a, b, c]
+    const LetterRandom = list[generateRandomIndex()]
+    const [Correcta, setCorrect] = useState(0)
+    const [Fallaste, setFalse] = useState(0)
+    const navigate = useNavigate('/levelkids')
+function handleClick(e, LetterRandom){
+    if (LetterRandom ===  e){
+        setCorrect(Correcta + 1);
+        alert(`true, ${ Correcta } ${ e }`);
+    }
+    if (LetterRandom !==  e) {
+        setFalse(Fallaste + 1);
+        alert(`false, ${ Fallaste } ${ e }`);
+    }
+    if (Correcta === 6){
+        navigate('/loginkids')
+    }
+        }
     return(
         <main className='level1'>
                 <div>
                     <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
+                        <p className="text_anime">{ LetterRandom }</p>
                     </header>
                 </div>
                 <BotonCentrado>
                     <BotonCentrado>
                         <img src={ speaker1 } alt='Kidfuns'/>
                         <p>Selecciona la correcta</p>
-                        <button type="button">a</button>
-                        <button type="button">z</button>
-                        <button type="button">m</button>
+                        <button type="button" onClick={() => {handleClick( a, LetterRandom )}} className='glow-on-hover' style={{ color: 'blue' }}>{ a }</button>
+                        <button type="button" onClick={() => {handleClick( b, LetterRandom )}} className='glow-on-hover' style={{ color: 'blue' }}>{ b }</button>
+                        <button type="button" onClick={() => {handleClick( c, LetterRandom )}} className='glow-on-hover' style={{ color: 'blue' }}>{ c }</button>
                     </BotonCentrado>
                 </BotonCentrado>
                 <div>
@@ -113,20 +153,44 @@ function Component4 ({ logo, numero, mano, micro, speaker, Formulario, conejo })
 }
 
 function Component5 ({ logo, speaker1, BotonCentrado, conejo1}) {
+    const a = generateRandomNumber()
+    const b = generateRandomNumber()
+    const c = generateRandomNumber()
+    const list = [ a, b, c]
+    const NumberRandom = list[generateRandomIndex()]
+    const [Correcta, setCorrect] = useState(0)
+    const [Fallaste, setFalse] = useState(0)
+    const navigate = useNavigate('/levelkids')
+function handleClick(e, NumberRandom){
+    if (NumberRandom ===  e){
+        setCorrect(Correcta + 1);
+        alert(`true, ${ Correcta } ${ e }`);
+    }
+    if (NumberRandom !==  e) {
+        setFalse(Fallaste + 1);
+        alert(`false, ${ Fallaste } ${ e }`);
+    }
+    if (Correcta === 6){
+        navigate('/loginkids')
+    }
+        }
     return (
+        
         <main className='level1'>
                 <div>
                     <header>
                         <img src={ logo } alt='Kidfuns' className='logoprincipal'/>
+                        <br></br>
+                        <h1>{ NumberRandom }</h1>
                     </header>
                 </div>
                 <BotonCentrado>
                     <BotonCentrado>
                         <img src={ speaker1 } alt='Kidfuns'/>
                         <p>Selecciona la correcta</p>
-                        <button type="button">3</button>
-                        <button type="button">1</button>
-                        <button type="button">2</button>
+                        <button type="button" onClick={() => {handleClick( a, NumberRandom )}}>{ a }</button>
+                        <button type="button" onClick={() => {handleClick( b, NumberRandom )}}>{ b }</button>
+                        <button type="button" onClick={() => {handleClick( c, NumberRandom )}}>{ c }</button>
                     </BotonCentrado>
                 </BotonCentrado>
                 <div>
